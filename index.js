a2052968ef76a80920c2b8460292488dc7a7aa63
jeffreyxazer <jeffreyxazer@gmail.com>
2025-03-04 20:29:28 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  const left = parseInt(dodger.style.left, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const left = parseInt(dodger.style.left, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 5}px`;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
